2c1507f3e7491c282638e5a6bc7811c5d4f3fa34
Clement Moulin-Frier <clement.moulinfrier@gmail.com>
2014-12-10 18:36:38 +0100

    pbs upd for new avakas interface

--- make-jobs.py
+++ make-jobs.py
@@ -9,13 +9,16 @@ python experiment.py --log_dir {log_dir}
 """
 
 from subprocess import call
-import datetime
+import argparse
 import os
 
 if __name__ == '__main__':
-	date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        log_dir = 'logs/' + date
-        os.mkdir(log_dir)
-	with open('/tmp/expe.pbs', 'w') as f:
-		f.write(pbs.format(log_dir=log_dir, job_name=date))
-	call(["qsub", "/tmp/expe.pbs"])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir', type=str, required=True)
+    parser.add_argument('--n_expes', type=int, required=True)
+    args = parser.parse_args()
+    for i in range(args.n_expes):
+        job_name = os.path.basename(os.path.normpath(args.dir)) + '_' + str(i + 1)
+        with open('/tmp/expe.pbs', 'w') as f:
+            f.write(pbs.format(log_dir=args.dir, job_name=job_name))
+            call(["qsub", "/tmp/expe.pbs"])
