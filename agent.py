0d579c57729406ce464580c0d53648aeaf053dd9
Clement Moulin-Frier <clement.moulinfrier@gmail.com>
2014-12-10 18:36:10 +0100

    ag upd for new avakas interface

--- agent.py
+++ agent.py
@@ -1,10 +1,8 @@
 from numpy import zeros, ones, array
 
-from explauto.sensorimotor_model.nearest_neighbor import NearestNeighbor
 from explauto.utils.config import make_configuration
 from explauto.models.dmp import DmpPrimitive
 from explauto.utils import bounds_min_max
-from explauto import InterestModel
 from explauto.agent import Agent
 
 
@@ -20,10 +18,35 @@ class DmpAgent(Agent):
                                     type=dmp_type, ay=ones(n_dmps) * 1.)
 
     @classmethod
-    def from_settings(cls, n_bfs, starting_position, babbling_name, sm_name, im_name):
-        params = get_params(n_bfs, starting_position, babbling_name, sm_name, im_name)
-        return cls(**params)
+    def from_settings(cls, starting_position, n_bfs, sm, babbling, im,
+                      bfs_half_range, angle_half_range, s_mins, s_maxs):
+        n_dmps = len(starting_position)
 
+        default = zeros(n_dmps*(n_bfs+2))
+        default[:n_dmps] = starting_position
+        default[-n_dmps:] = starting_position
+
+        poppy_ag = {'m_mins': list([-bfs_half_range] * (n_dmps * n_bfs)) +
+                    list(default[:n_dmps] - angle_half_range),
+
+                    'm_maxs': list([bfs_half_range] * (n_dmps * n_bfs)) +
+                    list(default[:n_dmps] + angle_half_range),
+
+                    's_mins': s_mins,
+                    's_maxs': s_maxs
+                    }
+
+        ag_conf = make_configuration(**poppy_ag)
+
+        sm_model = sm['cls'](ag_conf, **sm['config'])
+
+        im_dims = ag_conf.m_dims if babbling == 'motor' else ag_conf.s_dims
+        im_model = im['cls'](ag_conf, im_dims, **im['config'])
+
+        used = array([False]*n_dmps + [True]*(n_dmps*n_bfs) + [True]*n_dmps)
+
+        return cls(n_dmps=n_dmps, n_bfs=n_bfs, used=used, default=default,
+                   conf=ag_conf, sm=sm_model, im=im_model)
 
     def motor_primitive(self, m):
         self.m = bounds_min_max(m, self.conf.m_mins, self.conf.m_maxs)
@@ -33,39 +56,3 @@ class DmpAgent(Agent):
 
     def sensory_primitive(self, s):
         return s[-1]  # array([mean(s)]) #s[[-1]]
-
-
-sms = {
-    'knn': (NearestNeighbor, {'sigma_ratio': 1. / 38}),
-}
-
-def get_params(n_bfs, starting_position, babbling_name, sm_name, im_name):
-    n_dmps = len(starting_position)
-
-    default = zeros(n_dmps*(n_bfs+2))
-    default[:n_dmps] = starting_position
-    default[-n_dmps:] = starting_position
-
-    poppy_ag = {'m_mins': list([-600] * (n_dmps * n_bfs)) + list(default[:n_dmps] - 180.),
-                'm_maxs': list([600] * (n_dmps * n_bfs)) + list(default[:n_dmps] + 180.),
-                's_mins': [-1., -0.7, -0.1],
-                's_maxs': [1., 0.7, 0.7]
-                }
-
-    poppy_ag_conf = make_configuration(**poppy_ag)
-
-    im_dims = poppy_ag_conf.m_dims if babbling_name == 'motor' else poppy_ag_conf.s_dims
-    im = InterestModel.from_configuration(poppy_ag_conf, im_dims, im_name)
-
-    sm_cls, kwargs = sms[sm_name]
-    sm = sm_cls(poppy_ag_conf, **kwargs)
-
-    used = array([False]*n_dmps + [True]*(n_dmps*n_bfs) + [True]*n_dmps)
-    return {'n_dmps': n_dmps,
-            'n_bfs': n_bfs,
-            'used': used,
-            'default': default,
-            'conf': poppy_ag_conf,
-            'sm': sm,
-            'im': im
-           }
